05328639b107f070b81c2d056fefa3c253e5b55c
bmotta1506 <bmotta1506@gmail.com>
2025-12-17 15:10:54 -0500

    Ejercicio01

--- LABORATORIO22/Ejercicio03/Ejercicio03.py
+++ LABORATORIO22/Ejercicio03/Ejercicio03.py
@@ -1,5 +1,11 @@
 import requests
-
-r = requests.get("https://httpbin.org/get")
-print("Status:", r.status_code)
-print("Headers OK")
+url =  "https://httpbin.org/get"
+response = requests.get(url)
+data = response.json()
+print("IP:")
+print(data["origin"])
+print("\nHeaders:")
+for key, value in data["headers"].items():
+    print(f"{key}: {value}")
+print("\nArgs:")
+print(data["args"]) 
